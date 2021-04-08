0392b7c1ca89001c0d0fe03234de69b35ffd64c6
jakub <xivan@stuba.sk>
2021-04-08 19:04:31 +0200

    Test event p7 radiobutton1

--- src/main/java/sk/stuba/fei/uim/oop/TestFrame.java
+++ src/main/java/sk/stuba/fei/uim/oop/TestFrame.java
@@ -5,37 +5,39 @@ import java.awt.event.*;
 import javax.swing.*;
 
 
-class TestFrame extends JFrame implements ActionListener
-{	String Close="Zavri";
-    String Novinka = "New"; // tu pridame novy atribut
-    public static int pocetframeov = 0; // tu pridame pocitadlo
-    public static int umiestnenie = 10;
+class TestFrame extends JFrame
+        implements ItemListener
+{   JRadioButton one;
+    JRadioButton two;
+    ButtonGroup bg = new ButtonGroup();
+    JTextField In;
+
     public TestFrame ()
-    {	super("Test Frame");
-        setSize(300,300);
+    {   super("Test");
+        setLayout(new BorderLayout());
+        setSize(500,400);
 
-        JPanel P = new JPanel(); // Panel for Button
-        JButton B = new JButton(Close);
-        JButton novy = new JButton(Novinka); // tu pridame novy button
-        B.addActionListener(this); // Sam sebe Event-Listener
-        novy.addActionListener(this);
-        P.add(B); // Button na Panel
-        P.add(novy); // Button na Panel
-        add(P); // Panel na Fenster
-        setLocation(umiestnenie, umiestnenie); // tu nastavime umiestnenie okna
+        one = new JRadioButton("one");
+        add("North",one);
+        one.addItemListener(this);
+        bg.add(one);
+
+        two = new JRadioButton("two", true);
+        add("Center", two);
+        two.addItemListener(this);
+        bg.add(two);
+        add("South",In = new JTextField());
+        In.setText("One is " + one.isSelected() + " and two is " + two.isSelected());
+        pack();
         setVisible(true);
-        pocetframeov++; // tu zvysime pocet okien
-        umiestnenie = umiestnenie + 50; // tu posunieme umiestnenie pre dalsie okno
     }
 
-    public void actionPerformed (ActionEvent e)
-    {	if (e.getActionCommand().equals(Close))
-    {	if (pocetframeov == 1)System.exit(0); // tu ak je okno iba jedno ukonci program
-    else {dispose();
-        pocetframeov--;}	// Zavrie okno
+    public void itemStateChanged (ItemEvent e)
+    {   if (e.getSource()==one)
+    {   In.setText("One is " + one.isSelected() + " and two is " + two.isSelected());
     }
-        if (e.getActionCommand().equals(Novinka))
-        {	new TestFrame(); // nove okno
-        }
+    else if (e.getSource()==two)
+    {   In.setText("Two is " + two.isSelected() + " and one is " + one.isSelected());
+    }
     }
 }
