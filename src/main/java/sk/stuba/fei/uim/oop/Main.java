8cbb4af040256620d5fb835aabf5272c9bed2d73
jakub <xivan@stuba.sk>
2021-04-08 14:45:45 +0200

    Test 2

--- src/main/java/sk/stuba/fei/uim/oop/Main.java
+++ src/main/java/sk/stuba/fei/uim/oop/Main.java
@@ -1,11 +1,14 @@
 package sk.stuba.fei.uim.oop;
-
 import javax.swing.*;
+import java.awt.*;
+
 public class Main {
     public static void main (String args[])
     {
-        JFrame F = new JFrame();
+        JFrame F=new JFrame();
         F.setSize(300,300);
-        F.setVisible(true); }
-        //koment
+        F.setVisible(true);
+        Graphics g=F.getGraphics();
+        g.drawString("Hello World!",100,200);
+    }
 }
