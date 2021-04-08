8a8f68b00f2c2a5b7fdf2eddb50d0194a1df488b
jakub <xivan@stuba.sk>
2021-04-08 19:20:20 +0200

    Test event listzoznam

--- src/main/java/sk/stuba/fei/uim/oop/TestFrame.java
+++ src/main/java/sk/stuba/fei/uim/oop/TestFrame.java
@@ -6,40 +6,47 @@ import javax.swing.*;
 
 
 class TestFrame extends JFrame implements ItemListener
+{	List zoznam;
+    Choice rozbalovacie_volby;
+    String polozky_pre_zoznam[]={"Jablko","Hruska","Ceresna", "Melon", "Ananas"};
+    String polozky_pre_rozbalovacie[]={"Jablko","Hruska","Ceresna", "Melon", "Ananasik"};
 
-{   JCheckBox one;
-    JCheckBox two;
-    JTextField In;
 
     public TestFrame ()
-    {   super("Test");
+    {
+        super("Test Choicu a Listu");
         setLayout(new BorderLayout());
         setSize(500,400);
 
-        add("North", one = new JCheckBox("one", true)); one.addItemListener(this);
+        rozbalovacie_volby=new Choice();
+        rozbalovacie_volby.addItemListener(this);
 
-        add("Center", two = new JCheckBox("two")); two.addItemListener(this);
+        zoznam=new List();
+        zoznam.addItemListener(this);
 
-        add("South",In = new JTextField());
+        for (int i=0; i<polozky_pre_zoznam.length; i++)
+        {
+            //rozbalovacie_volby.add(polozky[i]);
+            zoznam.add(polozky_pre_zoznam[i]);
+        }
 
-        In.setText("One is " + one.isSelected() + " and two is " + two.isSelected());
+        for (int i=0; i<polozky_pre_rozbalovacie.length; i++)
+        {
+            rozbalovacie_volby.add(polozky_pre_rozbalovacie[i]);
+            //zoznam.add(polozky_pre_rozbalovacie[i]);
+        }
 
-        pack();
-
+        add("Center",zoznam);
+        add("South",rozbalovacie_volby);
         setVisible(true);
-
     }
-
-
 
     public void itemStateChanged (ItemEvent e)
-    {   if ((e.getSource()==one)||(e.getSource()==two))
-    {
-        In.setText("One is " + one.isSelected() + " and two is " + two.isSelected());
+    {	if (e.getSource()==rozbalovacie_volby)
+    {	zoznam.select(rozbalovacie_volby.getSelectedIndex());
     }
-    /*else if (e.getSource()==two)
-    {
-        In.setText("Two is " + two.isSelected() + " and one is " + one.isSelected());
-    }*/
+    else if (e.getSource()==zoznam)
+    {	rozbalovacie_volby.select(zoznam.getSelectedIndex());
+    }
     }
 }
