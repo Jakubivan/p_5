0cb03e5c154fa6265240a4d25e0ff9d79460c1de
jakub <xivan@stuba.sk>
2021-04-08 16:15:05 +0200

    Test event p6 nove okna cez button

--- src/main/java/sk/stuba/fei/uim/oop/TestFrame.java
+++ src/main/java/sk/stuba/fei/uim/oop/TestFrame.java
@@ -5,24 +5,30 @@ import java.awt.event.*;
 import javax.swing.*;
 
 class TestFrame extends JFrame implements ActionListener
-{	String Close="Zavri2";
-    JButton B; // tu je pridany atribut
+{	String Close="Zavri";
+    String Novinka = "New"; // tu pridame novy atribut
 
     public TestFrame ()
     {	super("Test Frame");
         setSize(300,300);
 
-        JPanel P = new JPanel(); // Panel pre Button
-        B = new JButton(Close); // tu pouzijeme pridany atribut
-        B.addActionListener(this); // Sam je Event-Listener
+        JPanel P = new JPanel(); // Panel for Button
+        JButton B = new JButton(Close);
+        JButton novy = new JButton(Novinka); // tu pridame novy button
+        B.addActionListener(this); // Sam sebe Event-Listener
+        novy.addActionListener(this);
         P.add(B); // Button na Panel
-        add(P); // Panel do Okna
+        P.add(novy); // Button na Panel
+        add(P); // Panel na Fenster
         setVisible(true);
     }
 
     public void actionPerformed (ActionEvent e)
-    {	if (e.getSource() == B) // tu je zmena
-    {	dispose(); System.exit(0); // zavrie okno a ukonci program
+    {	if (e.getActionCommand().equals(Close))
+    {	dispose(); System.exit(0); // Zavrie okno a ukonci program
     }
+        if (e.getActionCommand().equals(Novinka))
+        {	new TestFrame(); // nove okno
+        }
     }
 }
