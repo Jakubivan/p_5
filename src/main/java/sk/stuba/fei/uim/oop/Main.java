097cabd50c47c3ac0ce94ba8d954c52be508cc34
jakub <xivan@stuba.sk>
2021-04-08 15:10:01 +0200

    Test 5

--- src/main/java/sk/stuba/fei/uim/oop/Main.java
+++ src/main/java/sk/stuba/fei/uim/oop/Main.java
@@ -5,9 +5,16 @@ import java.awt.*;
 public class Main{
 
     public static void main (String args[]) {
-        JFrame F = new JFrame();
+        JFrame F=new JFrame();
         F.setSize(300,300);
+        F.setLayout(new BorderLayout());
+        F.add("North",new JLabel("Priklad s farbami"));
         F.add("Center",new TestCanvas());
+        JPanel p=new JPanel();
+
+        p.add(new JButton("Change"));
+        p.add(new JButton("Close"));
+        F.add("South",p);
         F.setVisible(true);
-        }
+    }
 }
