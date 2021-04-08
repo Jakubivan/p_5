73d4c935f7bd09a000507d01aa938979ac30ed4a
jakub <xivan@stuba.sk>
2021-04-08 16:17:18 +0200

    Test event p6 nove okna +postupne vypinanie cez button

--- src/main/java/sk/stuba/fei/uim/oop/TestFrame.java
+++ src/main/java/sk/stuba/fei/uim/oop/TestFrame.java
@@ -4,10 +4,12 @@ import java.awt.*;
 import java.awt.event.*;
 import javax.swing.*;
 
+
 class TestFrame extends JFrame implements ActionListener
 {	String Close="Zavri";
     String Novinka = "New"; // tu pridame novy atribut
-
+    public static int pocetframeov = 0; // tu pridame pocitadlo
+    public static int umiestnenie = 10;
     public TestFrame ()
     {	super("Test Frame");
         setSize(300,300);
@@ -20,12 +22,17 @@ class TestFrame extends JFrame implements ActionListener
         P.add(B); // Button na Panel
         P.add(novy); // Button na Panel
         add(P); // Panel na Fenster
+        setLocation(umiestnenie, umiestnenie); // tu nastavime umiestnenie okna
         setVisible(true);
+        pocetframeov++; // tu zvysime pocet okien
+        umiestnenie = umiestnenie + 50; // tu posunieme umiestnenie pre dalsie okno
     }
 
     public void actionPerformed (ActionEvent e)
     {	if (e.getActionCommand().equals(Close))
-    {	dispose(); System.exit(0); // Zavrie okno a ukonci program
+    {	if (pocetframeov == 1)System.exit(0); // tu ak je okno iba jedno ukonci program
+    else {dispose();
+        pocetframeov--;}	// Zavrie okno
     }
         if (e.getActionCommand().equals(Novinka))
         {	new TestFrame(); // nove okno
