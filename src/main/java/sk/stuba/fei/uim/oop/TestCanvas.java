0287c5aa1bb8f71be9191ac4ea5bd7151e40aa19
jakub <xivan@stuba.sk>
2021-04-08 15:36:33 +0200

    Test stvorec2

--- src/main/java/sk/stuba/fei/uim/oop/TestCanvas.java
+++ src/main/java/sk/stuba/fei/uim/oop/TestCanvas.java
@@ -5,26 +5,21 @@ import java.util.*;
 
 class TestCanvas extends Canvas
 {
-    public HashSet<Rectangle> OurObjects = new HashSet<>();
+    public HashSet<Rectangle> OurObjects = new HashSet<Rectangle>();
     public void NewObjects ()
     {	int i = 0;
         int j = 20;
-        while (i<8)
+        while (i<5)
         {	OurObjects.add(new Rectangle(10,j,20,20));
             j = j + 30; i++;
         }
     }
 
     public void paint (Graphics g)
-    {	Rectangle ActRect;
-        Iterator it = OurObjects.iterator();
-        while (it.hasNext())
-        {	Object Actual = it.next();
-            if (Actual instanceof Rectangle)
-            {	ActRect = (Rectangle) Actual;
-                System.out.println(ActRect.y);
-                g.drawRect(ActRect.x, ActRect.y, ActRect.width, ActRect.height);
-            }
-        }
+    {	for (Rectangle ActRect : OurObjects)
+    {
+        System.out.println(ActRect.y);
+        g.drawRect(ActRect.x, ActRect.y, ActRect.width, ActRect.height);
+    }
     }
 }
